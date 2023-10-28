bd97a66bb84c7c1d162fb7c907e5907bc745e57b
Nuñez <carlos.nunez.calvario@yandex.com>
2023-10-28 11:52:27 -0700

    Fixing types

--- source/router/Component_Template.ts
+++ source/router/Component_Template.ts
@@ -1,5 +1,7 @@
 export class ComponentTemplate<Type>{
-    RouteName: string;
-    State: <Type>;
+    RouteName: string = "";
+    /// @ts-ignore
+    State: Type;
+    // @ts-ignore
     render(): Promise<HTMLElement> {}
 }
