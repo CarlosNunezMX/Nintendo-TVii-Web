46af073c04a2ecb11ba92baf9deaf6e1ef6109a9
Nuñez <carlos.nunez.calvario@yandex.com>
2023-11-03 12:05:58 -0700

    Creating tools...

---
+++ tools/css_tools.ts
@@ -0,0 +1,11 @@
+import fs from "fs"
+import path from "path";
+
+/**
+ * In this tool we'll recive a path, and we'll read all
+ * css files in that, then extract the backgrounds for 
+ * download all base64 text and transform into images,
+ * then put it in their css files.
+ */
+
+
